a4c8ecc50e02655c3d73078a3138edcffd7283a6
Eamon Woortman <eamon.woortman@gmail.com>
2017-08-10 16:59:33 +0200

    Refactored the Facilitator class based on the Facilitator example from noblewhale/RakNetZebStyle

--- Facilitator.cpp
+++ Facilitator.cpp
@@ -23,7 +23,8 @@
 #include <time.h>
 #endif
 
-NatPunchthroughServer natPunchthrough;
+RakPeerInterface *peer;
+NatPunchthroughServer *natPunchthrough;
 bool quit;
 bool printStats = false;
 
@@ -31,11 +32,29 @@ char* logfile = "facilitator.log";
 const int fileBufSize = 1024;
 char pidFile[fileBufSize];
 
+
 void shutdown(int sig)
 {
 	Log::print_log("Shutting down\n\n");
 	quit = true;
 }
+
+
+void cleanup()
+{
+	if (pidFile)
+	{
+		if (remove(pidFile) != 0)
+			fprintf(stderr, "Failed to remove PID file at %s\n", pidFile);
+	}
+	peer->Shutdown(100, 0);
+	peer->DetachPlugin(natPunchthrough);
+	delete natPunchthrough;
+	natPunchthrough = NULL;
+	RakNetworkFactory::DestroyRakPeerInterface(peer);
+	Log::print_log("Quitting\n");
+}
+
 
 void usage()
 {
@@ -46,6 +65,8 @@ void usage()
 		   "-e\tDebug level (0=OnlyErrors, 1=Warnings, 2=Informational(default), 2=FullDebug)\n\t"
 		   "-c\tMax connection count. [1000]\n\t"
 		   "-s\tStatistics print delay (in seconds). [0]\n\n"
+		   "-h\tBind to listen address\n\t"
+		   "-b\tBind a second (external) addresses. When using 2 IP addresses, NATPunchthroughServer supports port stride detection, improving success rate\n\n"
 		   "If any parameter is omitted the default value is used.\n");
 }
 
@@ -94,14 +115,42 @@ void ProcessPacket(Packet *packet)
 	}
 }
 
+bool validate_ip(char* ip)
+{
+	bool valid = true;
+	int i = 0;
+	int dotCount = 0;
+	while (ip[i] != 0)
+	{
+		if (ip[i++] == '.')
+			dotCount++;
+	}
+	if (dotCount != 3)
+		valid = false;
+
+#ifdef WIN32
+	unsigned long ulAddr = INADDR_NONE;
+	ulAddr = inet_addr(ip);
+	if (valid && ulAddr != INADDR_NONE)
+		return true;
+#else
+	struct in_addr ia;
+	if (valid && inet_aton(ip, &ia) == 1)
+		return true;
+#endif
+	printf("IP address '%s' is invalid\n", ip);
+	return false;
+}
+
 int main(int argc, char *argv[])
 {  
 #ifndef WIN32
 	setlinebuf(stdout);
 #endif
 
-	RakPeerInterface *peer = RakNetworkFactory::GetRakPeerInterface();	// The facilitator
-	
+	peer = RakNetworkFactory::GetRakPeerInterface();	// The facilitator
+	natPunchthrough = new NatPunchthroughServer;
+
 	// Default values
 	int facilitatorPort = 50005;
 	int connectionCount = 1000;
@@ -112,7 +161,9 @@ int main(int argc, char *argv[])
 	bool useLogFile = false;
 	int statDelay = 30;
 	bool daemonMode = false;
-			
+	char *bindToIP1 = NULL;
+	char *bindToIP2 = NULL;
+
 	// Process command line arguments
 	for (int i = 1; i < argc; i++)
 	{
@@ -177,6 +228,36 @@ int main(int argc, char *argv[])
 					printStats = true;
 					break;
 				}
+				case 'h':
+				{
+					bindToIP1 = argv[i + 1];
+					i++;
+					if (!validate_ip(bindToIP1))
+					{
+						fprintf(stderr, "Invalid IP address given in -h parameter.\n");
+						return 1;
+					}
+					break;
+				}
+				case 'b':
+				{
+					if (argc - i < 2)
+					{
+						fprintf(stderr, "You must provide an IP address with -b parameter.\n");
+						return 1;
+					}
+
+					bindToIP2 = argv[i + 1];
+					
+					if (!validate_ip(bindToIP2))
+					{
+						fprintf(stderr, "Invalid IP address with -b paramter. Must be in dot notation.\n");
+						return 1;
+					}
+							
+					i = i + 1;
+					break;
+				}
 				case '?':
 				{
 					usage();
@@ -211,19 +292,61 @@ int main(int argc, char *argv[])
 #endif
 
 	peer->SetMaximumIncomingConnections(connectionCount);
+
 	char ipList[ MAXIMUM_NUMBER_OF_INTERNAL_IDS ][ 16 ];
 	unsigned int binaryAddresses[MAXIMUM_NUMBER_OF_INTERNAL_IDS];
 	SocketLayer::Instance()->GetMyIP( ipList, binaryAddresses );
-	//SocketDescriptor sd(facilitatorPort,ipList[0]);
-	SocketDescriptor sd(facilitatorPort,0);
-	if (!peer->Startup(connectionCount, 30, &sd, 1))
+
+	int ipCount = 0;
+	int i = 0;
+	while (i < MAXIMUM_NUMBER_OF_INTERNAL_IDS)
+	{
+		if (ipList[i][0] != 0 && strcmp(ipList[i], "127.0.0.1") != 0)
+			ipCount++;
+		i++;
+	}
+
+	if (ipCount < 1)
+	{
+		Log::error_log("Not enough IP addresses to bind to.\n");
+		Log::error_log("To run the facilitator you need at least one public IP addresses available.\n\n");
+		cleanup();
+		return 1;
+	}
+
+	if (!bindToIP1)
+		bindToIP1 = ipList[0];
+
+	int sdLen = 1;
+	SocketDescriptor sd[2];
+
+	sd[0].port = facilitatorPort;
+	strcpy(sd[0].hostAddress, bindToIP1);
+
+	if (bindToIP2 != NULL)
+	{
+		sd[1].port = sd[0].port + 1;
+		strcpy(sd[1].hostAddress, bindToIP2);
+		sdLen = 2;
+	}
+	
+	if (!peer->Startup(8096, 10, sd, sdLen))
 	{
 		Log::error_log("Failed to start RakPeer!\n");
 		RakNetworkFactory::DestroyRakPeerInterface(peer);
 		return 1;
 	}
-	peer->AttachPlugin(&natPunchthrough);
-	
+	peer->SetTimeoutTime(5000, UNASSIGNED_SYSTEM_ADDRESS);
+	peer->AttachPlugin(natPunchthrough);
+
+	if (bindToIP2) {
+		printf("Facilitator started on %s:%d and %s:%d\n", bindToIP1, sd[0].port, bindToIP2, sd[1].port);
+	}
+	else {
+		printf("Started on %s:%d\n", bindToIP1, sd[0].port);
+	}
+	printf("\n");
+
 	Log::print_log("Unity Facilitator version 2.0.0\n");
 	Log::print_log("Listen port set to %d\n",facilitatorPort);
 	Log::print_log("%d connection count limit\n", connectionCount);
@@ -272,6 +395,14 @@ int main(int argc, char *argv[])
 		if (remove(pidFile) != 0)
 			fprintf(stderr, "Failed to remove PID file at %s\n", pidFile);
 	}
+
+	if (natPunchthrough) 
+	{
+		peer->DetachPlugin(natPunchthrough);
+		delete natPunchthrough;
+	}
+
+	natPunchthrough = NULL;
 	peer->Shutdown(100,0);
 	RakNetworkFactory::DestroyRakPeerInterface(peer);
 				
